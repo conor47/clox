6595f2f5add41e41ac085ed38d47ff7ac9d2deaf
Conor Ladrigan <conorladrigan95@gmail.com>
2024-02-10 20:21:26 +0000

    Initial implementation of VM stack and associated functions

--- src/vm.c
+++ src/vm.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "debug.h"
 #include "vm.h"
 
 VM vm;
 
+static void resetStack() {
+    vm.stackTop = vm.stack;
+}
+
 void initVM() {
+    resetStack();
 }
 
 void freeVM() {
+}
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
 }
 
 static InterpretResult run() {
@@ -16,6 +32,9 @@ static InterpretResult run() {
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
     
     for(;;) {
+        #ifdef DEBUG_TRACE_EXECUTION
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+        #endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
