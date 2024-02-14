0fca4221ddcb43199c3f1a34b8a0cf623f60018d
Conor Ladrigan <conorladrigan95@gmail.com>
2024-02-14 14:13:25 +0000

    Implement block statements. Implement functions for handling scope creation and deletion

--- src/compiler.c
+++ src/compiler.c
@@ -139,6 +139,14 @@ static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void block() {
+    while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+        declaration();
+    }
+    
+    consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
 static void varDeclaration() {
     uint8_t global = parseVariable("Expect variable name");
     
@@ -200,6 +208,10 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else if (match(TOKEN_LEFT_BRACE)) {
+        beginScope();
+        block();
+        endScope();
     } else {
         expressionStatement();
     }
@@ -234,7 +246,13 @@ static void endCompiler() {
     #endif
 }
 
+static void beginScope()  {
+    current->scopeDepth++;
+}
 
+static void endScope() {
+    current->scopeDepth--;
+}
 
 static void binary(bool canAssign) {
     TokenType operatorType = parser.previous.type;
