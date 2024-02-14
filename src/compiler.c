948dd6544a0598f53ac77b9ddf25bc55e110d76f
Conor Ladrigan <conorladrigan95@gmail.com>
2024-02-14 18:19:40 +0000

    Implement for statements

--- src/compiler.c
+++ src/compiler.c
@@ -155,6 +155,10 @@ static void parsePrecedence(Precedence precedence);
 static void markInitialized();
 static int resolveLocal(Compiler* compiler, Token* name);
 static void declareVariable();
+static void patchJump(int offset);
+static void beginScope();
+static void endScope();
+static void and_(bool canAssign);
 
 static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
@@ -203,6 +207,55 @@ static void expressionStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
     emitByte(OP_POP);
+}
+
+static void forStatement() {
+    beginScope();
+    
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+    if (match(TOKEN_VAR)) {
+        varDeclaration();
+    } else if (match(TOKEN_SEMICOLON)) {
+        // No initializer
+    } else {
+        expressionStatement();
+    }
+    
+    int loopStart = currentChunk()->count;
+
+    int exitJump = -1;
+    if (!match(TOKEN_SEMICOLON)) {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+        // Jump out of the loop if the condition is false.
+        exitJump = emitJump(OP_JUMP_IF_FALSE);
+        emitByte(OP_POP);
+    }
+
+    if (!match(TOKEN_RIGHT_PAREN)) {
+        int bodyJump = emitJump(OP_JUMP);
+
+        int incrementStart = currentChunk()->count;
+        expression();
+        emitByte(OP_POP);
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        emitLoop(loopStart);
+        loopStart = incrementStart;
+        patchJump(bodyJump);
+    }
+    
+    statement();
+
+    emitLoop(loopStart);
+
+    if (exitJump != -1) {
+        patchJump(exitJump);
+        emitByte(OP_POP);
+    }
+
+    endScope();
 }
 
 static void printStatement() {
@@ -276,6 +329,8 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else if (match(TOKEN_FOR)) {
+        forStatement();
     } else if (match(TOKEN_IF)) {
         ifStatement();
     } else if (match(TOKEN_WHILE)) {
