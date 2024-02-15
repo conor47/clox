e6b48b978afb4e7dc46f4bcde46ebe904b43ac6a
Conor Ladrigan <conorladrigan95@gmail.com>
2024-02-15 09:37:12 +0000

    Being implementation of compiling function declarations

--- src/compiler.c
+++ src/compiler.c
@@ -197,6 +197,27 @@ static void block() {
     consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
 }
 
+static void function(FunctionType type) {
+    Compiler compiler;
+    initCompiler(&compiler, type);
+    beginScope();
+    
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+    consume(TOKEN_LEFT_BRACE, "Expect '{' before a function body.");
+    block();
+
+    ObjFunction* function = endCompiler();
+    emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+}
+
+static void funDeclaration() {
+    uint8_t global = parseVariable("Expect function name.");
+    markInitialized();
+    function(TYPE_FUNCTION);
+    defineVariable(global);
+}
+
 static void varDeclaration() {
     uint8_t global = parseVariable("Expect variable name");
     
@@ -324,6 +345,9 @@ static void endScope() {
 }
 
 static void declaration() {
+    if (match(TOKEN_FUN)) {
+        funDeclaration();
+    }
     if (match(TOKEN_VAR)) {
         varDeclaration();
     } else {
@@ -639,6 +663,7 @@ static uint8_t parseVariable(const char* errorMessage) {
 }
 
 static void markInitialized() {
+    if (current->scopeDepth == 0) return;
     current->locals[current->localCount - 1].depth = current->scopeDepth;
 }
 
