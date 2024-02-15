dbb63a49f6f3bde6b4bbdf5045b3ec84b4e48d8c
Conor Ladrigan <conorladrigan95@gmail.com>
2024-02-15 10:12:48 +0000

    handle function call errors including stack overflow and incorrect arguement count. Add support for printing stack traces

--- src/vm.c
+++ src/vm.c
@@ -24,10 +24,18 @@ static void runTimeError(const char* format, ...) {
     va_end(args);
     fputs("\n", stderr);
     
-    CallFrame* frame = &vm.frames[vm.frameCount - 1];
-    size_t instruction = frame->ip - frame->function->chunk.code -1;
-    int line = frame->function->chunk.lines[instruction];
-    fprintf(stderr, "[line %d] in script \n", line);
+    for (int i = vm.frameCount - 1; i >= 0; i--) {
+        CallFrame* frame = &vm.frames[i];
+        ObjFunction* function = frame->function;
+        size_t instruction = frame->ip - function->chunk.code - 1;
+        fprintf(stderr, "[line %d] in", function->chunk.lines[instruction]);
+        if (function->name == NULL) {
+            fprintf(stderr, "script\n");
+        } else {
+            fprtinf(stderr, "%s()\n", function->name->chars);
+        }
+    }
+
     resetStack();
 }
 
@@ -60,6 +68,16 @@ static Value peek(int distance) {
 }
 
 static bool call(ObjFunction* function, int argCount) {
+    if (argCount != function->arity) {
+        runTimeError("Expected %d arguments but got %d.", function->arity, argCount);
+        return false;
+    }
+
+    if (vm.frameCount == FRAMES_MAX) {
+        runTimeError("Stack overflow.");
+        return false;
+    }
+
     CallFrame* frame = &vm.frames[vm.frameCount++];
     frame->function = function;
     frame->ip = function->chunk.code;
