9ff41d2140ff50fed19494c4fe7bb50a5056cb17
Conor Ladrigan <conorladrigan95@gmail.com>
2024-02-16 19:05:23 +0000

    Add compile time and run time support for basic Class declarations

--- src/compiler.c
+++ src/compiler.c
@@ -242,6 +242,18 @@ static void function(FunctionType type) {
     }
 }
 
+static void classDeclaration() {
+    consume(TOKEN_IDENTIFIER, "Expect class name.");
+    uint8_t nameConstant = identifierConstant(&parser.previous);
+    declareVariable();
+    
+    emitBytes(OP_CLASS, nameConstant);
+    defineVariable(nameConstant);
+    
+    consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
+    consume(TOKEN_RIGHT_BRACE, "Expect '}' before class body.");
+}
+
 static void funDeclaration() {
     uint8_t global = parseVariable("Expect function name.");
     markInitialized();
@@ -394,10 +406,11 @@ static void endScope() {
 }
 
 static void declaration() {
-    if (match(TOKEN_FUN)) {
+    if (match(TOKEN_CLASS)) {
+        classDeclaration();
+    } else if (match(TOKEN_FUN)) {
         funDeclaration();
-    }
-    if (match(TOKEN_VAR)) {
+    } else if (match(TOKEN_VAR)) {
         varDeclaration();
     } else {
         statement();
