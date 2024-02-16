d460487d66d74a63c67480c1ab3933d9d714976e
Conor Ladrigan <conorladrigan95@gmail.com>
2024-02-16 14:57:45 +0000

    Initial GC setup. Include two flags for GC debugging and logging

--- src/object.c
+++ src/object.c
@@ -15,6 +15,10 @@ static Obj* allocateObject(size_t size, ObjType type) {
     // insert object into VM linked list
     object->next = vm.objects;
     vm.objects = object;
+    
+    #ifdef DEBUG_LOG_GC
+        printf("%p allocate %zu for %d\n", (void*)object, size, type);
+    #endif
     return object;
 }
 
