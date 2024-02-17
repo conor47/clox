c9bb6d743501757be9f11b23ed291eb7f63137b5
Conor Ladrigan <conorladrigan95@gmail.com>
2024-02-17 16:17:30 +0000

    Forbid subclassing non class objects

--- src/vm.c
+++ src/vm.c
@@ -471,6 +471,11 @@ static InterpretResult run() {
                 break;
             case OP_INHERIT: {
                 Value superclass = peek(1);
+                if (!IS_CLASS(superclass)) {
+                    runTimeError("Superclass must be a class.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                
                 ObjClass* subclass = AS_CLASS(peek(0));
                 tableAddAll(&AS_CLASS(superclass)->methods, &subclass->methods);
                 pop();
